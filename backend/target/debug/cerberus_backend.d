69da828c298e6dc6de1c07872cfa266bf872ff59
CERBERUSCHAIN <tonyjames.staub@gmail.com>
2025-05-05 13:52:29 -0500

    Environment set up and servers running

---
+++ backend/target/debug/cerberus_backend.d
@@ -0,0 +1,1 @@
+C:\Users\lowke\OneDrive\Desktop\solana-bot-platform\backend\target\debug\cerberus_backend.exe: C:\Users\lowke\OneDrive\Desktop\solana-bot-platform\backend\src\main.rs
---
+++ backend/target/debug/deps/cerberus_backend.d
@@ -0,0 +1,5 @@
+C:\Users\lowke\OneDrive\Desktop\solana-bot-platform\backend\target\debug\deps\cerberus_backend.d: src\main.rs
+
+C:\Users\lowke\OneDrive\Desktop\solana-bot-platform\backend\target\debug\deps\cerberus_backend.exe: src\main.rs
+
+src\main.rs:
